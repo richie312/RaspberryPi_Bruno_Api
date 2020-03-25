6b38b5eecfa96e3017cf9725cf8c157ad55dabc2
richie312 <richie.chatterjee31@gmail.com>
2020-03-25 19:33:46 +0530

    added the env file dumper in ngrok checker file

--- bruno_app/ngrok_uri_checker.py
+++ bruno_app/ngrok_uri_checker.py
@@ -1,7 +1,24 @@
 import requests
+import json
+import os
+import dotenv
+main_dir = os.getcwd()
 ngrok_base_url = "http://127.0.0.1:4040/api"
 
-response = request.get(ngrok_base_url + '/tunnels')
+response = requests.get(ngrok_base_url + '/tunnels')
+response_obj = response.json()
 
-result = response.json()
-print(result)
+# get the uri for videostream and bruno_app
+
+bruno_app_url = response_obj['tunnels'][0]['public_url']
+video_stream_url = response_obj['tunnels'][0]['public_url']
+
+# dump the urls in the .env files
+
+# set the parent path for the dot_env
+dot_env_path = main_dir
+url_dict  = {'bruno_app_url' : bruno_app_url, 
+            'video_stream_url': video_stream_url}
+key_list = list(url_dict.keys())
+# generate the keys and save it in .env file
+[dotenv.set_key[dot_env_path,key_list[i],url_dict[key_list[i] for i in range(len(key_list))]
