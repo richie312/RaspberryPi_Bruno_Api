567a831f8bf6711006e547d317688ff6218397e6
richie312 <richie.chatterjee31@gmail.com>
2020-03-27 09:28:58 +0530

    added the pysftp file

--- bruno_app/sftp.py
+++ bruno_app/sftp.py
@@ -1,22 +1,9 @@
 
 """SFTP with Richie's Instance"""
 
-import paramiko
-import gzip
-user_name = 'Richie'
-host='192.168.43.186'
-ssh_client = paramiko.SSHClient()
-ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect(hostname = host,
-                   port = 22,
-                   username = user_name,
-                   key_filename='/home/pi/Downloads/bruno_app/.ssh/id_rsa.pub')
+import pysftp
 
-a,b,c = ssh_client.exec_command('hostname')
-print(b.readlines())
-sftp=ssh_client.open_sftp()
-
-remotepath = 'D:\RaspberryPi_Bruno_Api\bruno_app'
-localpath = '/home/pi/Downloads/bruno_app/readme.txt'
-sftp.get(remotepath,localpath)
-
+with pysftp.Connection('hostname', username='me', password='secret') as sftp:
+    with sftp.cd('D:\RaspberryPi_Bruno_Api\bruno_app'):           # temporarily chdir to allcode
+        sftp.put('/home/pi/Downloads/bruno_app/.env')  	# upload file to allcode/pycode on remote
+        #sftp.get('remote_file')         # get a remote file
--- sftp.py
+++
@@ -1,9 +0,0 @@
-
-"""SFTP with Richie's Instance"""
-
-import pysftp
-
-with pysftp.Connection('hostname', username='me', password='secret') as sftp:
-    with sftp.cd('D:\RaspberryPi_Bruno_Api\bruno_app'):           # temporarily chdir to allcode
-        sftp.put('/home/pi/Downloads/bruno_app/.env')  	# upload file to allcode/pycode on remote
-        #sftp.get('remote_file')         # get a remote file
