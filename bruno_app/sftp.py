e33a8c0c863cd90572694d7dbc37245a469cc6e3
richie312 <richie.chatterjee31@gmail.com>
2020-03-27 12:57:42 +0530

    added the id_rsa

--- bruno_app/sftp.py
+++ bruno_app/sftp.py
@@ -1,13 +1,13 @@
 import paramiko
 import gzip
 user_name = 'Richie'
-host='192.168.43.186'
+host='LAPTOP-25QB2DD1@192.168.43.186'
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(hostname = host,
                    port = 22,
                    username = user_name,
-                   key_filename='/home/pi/Downloads/bruno_app/.ssh/id_rsa.pub')
+                   key_filename='/home/pi/Downloads/bruno_app/.ssh/id_rsa')
 
 a,b,c = ssh_client.exec_command('hostname')
 print(b.readlines())
@@ -16,3 +16,7 @@ sftp=ssh_client.open_sftp()
 remotepath = 'D:\RaspberryPi_Bruno_Api\bruno_app'
 localpath = '/home/pi/Downloads/bruno_app/readme.txt'
 sftp.get(remotepath,localpath)
+
+ssh_client.connect(hostname = host,
+                   port = 22,
+                   username = user_name)
