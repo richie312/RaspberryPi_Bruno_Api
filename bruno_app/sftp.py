0ed84b17b92e6a81cca68cc26f33a0e475a0c6d9
richie312 <richie.chatterjee31@gmail.com>
2020-03-27 12:56:09 +0530

    added the paramiko file

--- bruno_app/sftp.py
+++ bruno_app/sftp.py
@@ -1,18 +1,21 @@
 import paramiko
 import gzip
-user_name = 'Richie'
-host='192.168.43.186'
+user_name = 'pi'
+host='192.168.43.82'
+with open(r'C:\Users\Richie\.ssh\passwd.txt', 'r') as file:    
+    passwd = file.readlines()
 ssh_client = paramiko.SSHClient()
+
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(hostname = host,
                    port = 22,
                    username = user_name,
-                   key_filename='/home/pi/Downloads/bruno_app/.ssh/id_rsa.pub')
+                   password = passwd[0])
 
 a,b,c = ssh_client.exec_command('hostname')
 print(b.readlines())
 sftp=ssh_client.open_sftp()
 
-remotepath = 'D:\RaspberryPi_Bruno_Api\bruno_app'
-localpath = '/home/pi/Downloads/bruno_app/readme.txt'
+remotepath = '/home/pi/Downloads/bruno_app/.env'
+localpath = 'D:\\RaspberryPi_Bruno_Api\\bruno_app\\.env'
 sftp.get(remotepath,localpath)
