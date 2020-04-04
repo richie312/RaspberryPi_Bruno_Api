7c53bbce128362bc8c9d9169a368ab38ce9afe21
richie312 <richie.chatterjee31@gmail.com>
2020-04-04 18:28:38 +0530

    modified the temperature file and ngrok uri checker file

--- bruno_app/ngrok_uri_checker.py
+++ bruno_app/ngrok_uri_checker.py
@@ -10,8 +10,20 @@ response_obj = response.json()
 
 # get the uri for videostream and bruno_app
 
-bruno_app_url = response_obj['tunnels'][3]['public_url']
-video_stream_url = response_obj['tunnels'][0]['public_url']
+# collect the index number for bruno app url (any one url)
+for i in range(len(response_obj['tunnels'])):
+    if response_obj['tunnels'][i]['name'] == 'bruno-app':
+        bruno_app_url_index = i
+
+bruno_app_url = response_obj['tunnels'][bruno_app_url_index]['public_url']
+
+# collect the index for the video stream_url
+
+for j in range(len(response_obj['tunnels'])):
+    if response_obj['tunnels'][j]['name'] == 'video-stream':
+        video_stream_url_index = j
+        
+video_stream_url = response_obj['tunnels'][video_stream_url_index]['public_url']
 
 # dump the urls in the .env files
 
--- raspberry_files/dht11_example.py
+++ raspberry_files/dht11_example.py
@@ -9,19 +9,6 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
-# Create the connection between raspbery pi and AWS RDS mysql
-
-host="richie-database.cml5lvgzqjbw.us-east-1.rds.amazonaws.com"
-port=3306
-dbname="RDS_MySql"
-user="richie31"
-password="Nirvikalpa!123"
-
-conn = pymysql.connect(host, user=user,port=port,
-                           passwd=password, db=dbname)
-
-cursor = conn.cursor()
-
 # read data using pin 14
 instance = dht11.DHT11(pin=23)
 
@@ -30,16 +17,4 @@ while True:
     if result.is_valid():
         print("Last valid input: " + str(datetime.datetime.now()))
         print("Temperature: %d C" % result.temperature)
-        print("Humidity: %d %%" % result.humidity)
-        """Insert Data into Raspberry Pi"""
-
-        sql_query = "INSERT INTO readings_temp (dateofreading, temperature, Humidity) VALUES (%s,%s,%s)"
-        time = datetime.datetime.now()
-        sql_date_format = time.strftime('%Y-%m-%d %H:%M:%S')
-        dateofreading = sql_date_format
-        temperature = result.temperature        
-        Humidity = result.humidity
-        val = (dateofreading, temperature, Humidity)
-        cursor.execute(sql_query,val)
-        conn.commit()
-    
+        print("Humidity: %d %%" % result.humidity)   
